4400219d33837f7ea374e3ba76e8b4de14adf372
pooja13196 <73737479+pooja13196@users.noreply.github.com>
2021-04-15 11:39:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dustbinObj, paperObject,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	
+
+	
+  
+}
+
+
+function draw() {
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+    
+  	}
+}
+
+
+
+
+
